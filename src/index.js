fe618672b4e5201cebed9fb4dbe2646b14aa7f67
richgkav <richgkav@gmail.com>
2020-06-23 10:48:09 +0100

    banner

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ function main() {
     const menu = document.getElementById('menu');
     menu.append(newDiv(null, 'menu-item', 'Walks'));
     menu.append(newDiv(null, 'menu-item', 'About Us'));
+
+    const logo = document.getElementById('logo');
+    logo.append(newDiv(null, 'logo-item', 'Antrim Walks'));
 }
 
 const newDiv = function _newDiv(idName, className, innerHTML) {
     const newDiv = document.createElement('div');
     if (idName) newDiv.id = idName;
     if (className) newDiv.classList.add(className);
+    newDiv.classList.add('items-design');
     if (innerHTML) newDiv.innerHTML = innerHTML;
     return newDiv;
 }
