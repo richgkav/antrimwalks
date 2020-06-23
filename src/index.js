7847925cdfc73c25118ad93b07c8a1c2ff73080e
richgkav <richgkav@gmail.com>
2020-06-23 22:55:53 +0100

    design improvements

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import "./index.css";
 
 function main() {
     const menu = document.getElementById('menu');
-    menu.append(newDiv(null, 'menu-item', 'Walks'));
+
+    const menuWalks = newDiv(null, 'menu-item', 'Walks');
+    addLink(menuWalks, '#walks-content');
+    menu.append(menuWalks);
+
     menu.append(newDiv(null, 'menu-item', 'About Us'));
 
     const logo = document.getElementById('logo');
-    logo.append(newDiv(null, 'logo-item', 'Antrim Walks'));
+    logo.append(newDiv(null, 'logo-item', 'Stick logo here'));
+
+    const banner = document.getElementById('banner');
+    banner.innerHTML = 'Great Walks in the County of Antrim<br>Northern Ireland';
 }
 
 const newDiv = function _newDiv(idName, className, innerHTML) {
@@ -17,6 +24,12 @@ const newDiv = function _newDiv(idName, className, innerHTML) {
     newDiv.classList.add('items-design');
     if (innerHTML) newDiv.innerHTML = innerHTML;
     return newDiv;
+}
+
+const addLink = function _addIDLink(element, id) {
+    const newA = document.createElement('a');
+    newA.setAttribute('href', id);
+    element.append(newA);
 }
 
 window.onload = main;
